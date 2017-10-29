934856664ae97e6ac450445ea71d6f4b1b3ac917
mishoryu <mishoryu@youkai.com>
2017-10-29 19:10:29 +1100

    Completed practice assignment solution.

--- week3-practice-assignment/module3_1_assignment.rb
+++ week3-practice-assignment/module3_1_assignment.rb
@@ -1,5 +1,21 @@
 require 'httparty'
 
 class Recipe
+  include HTTParty
 
+  @food2fork_key = ENV['FOOD2FORK_KEY']
+
+  disable_rails_query_string_format
+
+  base_uri "http://food2fork.com/api"
+  default_params key: @food2fork_key
+  format :json
+
+  def self.for (term)
+    begin
+      get('/search', query: {q: term})["recipes"]
+    rescue HTTParty::RedirectionTooDeep => e
+      puts e.response.body
+    end
+  end
 end
